e2d3a8981555adb7f838ac46347dc737f7060f1f
Bauke Scholtz <bauke.scholtz@kizitos.com>
2014-04-28 09:26:19 +0200

    Convert to Java project; Added Vdldoc generator; Copied all docs from Google Code host

---
+++ src/org/omnifaces/vdldoc/Generate.java
@@ -0,0 +1,33 @@
+package org.omnifaces.vdldoc;
+
+import java.io.File;
+
+
+/**
+ * Helper class to generate VDL documentation.
+ *
+ * @author Bauke Scholtz
+ */
+public class Generate {
+
+	private static final String VDLDOC_TITLE = "OmniFaces VDL Documentation - Generated Documentation";
+
+	public static void main(String[] args) throws Exception {
+		File omniFacesHomepageProject = new File("").getAbsoluteFile();
+		File omniFacesProject = new File(omniFacesHomepageProject.getParentFile(), "/omnifaces");
+
+		if (!omniFacesProject.exists()) {
+			throw new IllegalArgumentException("OmniFaces project missing in workspace");
+		}
+
+		VdldocGenerator generator = new VdldocGenerator();
+		generator.setWindowTitle(VDLDOC_TITLE);
+		generator.setDocTitle(VDLDOC_TITLE);
+		generator.setOutputDirectory(new File(omniFacesHomepageProject, "/docs/vdldoc/current"));
+		generator.setFacesConfig(new File(omniFacesProject, "/src/META-INF/faces-config.xml"));
+		generator.addTaglib(new File(omniFacesProject, "/src/META-INF/omnifaces-ui.taglib.xml"));
+		generator.addTaglib(new File(omniFacesProject, "/src/META-INF/omnifaces-functions.taglib.xml"));
+		generator.generate();
+	}
+
+}
